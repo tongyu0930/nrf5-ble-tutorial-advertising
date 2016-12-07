23b169cabe755ced8ae7492c04b43a3fb802f870
Tong Yu <yutong0815@gmail.com>
2016-12-08 00:12:15 +0100

    removed conn_params

--- main.c
+++ main.c
@@ -66,11 +66,6 @@
 #define APP_TIMER_PRESCALER              0                                          /**< Value of the RTC1 PRESCALER register. */
 #define APP_TIMER_OP_QUEUE_SIZE          4                                          /**< Size of timer operation queues. */
 
-//#define MIN_CONN_INTERVAL                MSEC_TO_UNITS(100, UNIT_1_25_MS)           /**< Minimum acceptable connection interval (0.1 seconds). */
-//#define MAX_CONN_INTERVAL                MSEC_TO_UNITS(200, UNIT_1_25_MS)           /**< Maximum acceptable connection interval (0.2 second). */
-//#define SLAVE_LATENCY                    0                                          /**< Slave latency. */
-//#define CONN_SUP_TIMEOUT                 MSEC_TO_UNITS(4000, UNIT_10_MS)            /**< Connection supervisory timeout (4 seconds). */
-
 #define FIRST_CONN_PARAMS_UPDATE_DELAY   APP_TIMER_TICKS(5000, APP_TIMER_PRESCALER) /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (5 seconds). */
 #define NEXT_CONN_PARAMS_UPDATE_DELAY    APP_TIMER_TICKS(30000, APP_TIMER_PRESCALER)/**< Time between each call to sd_ble_gap_conn_param_update after the first call (30 seconds). */
 #define MAX_CONN_PARAMS_UPDATE_COUNT     3                                          /**< Number of attempts before giving up the connection parameter negotiation. */
@@ -127,9 +122,7 @@ static void timers_init(void)
 static void gap_params_init(void)
 {
     uint32_t                err_code;
-    
-    // Declearing parameter structs. Try to go to the struct definitions to get more information about what parameters they contain
-//    ble_gap_conn_params_t   gap_conn_params; 	// Struct to store GAP connection parameters like max min connection interval etc
+
     ble_gap_conn_sec_mode_t sec_mode;			// Struct to store security parameters 
 
     // A simple macro that sets the Security Mode and Level bits in sec_mode
@@ -142,89 +135,12 @@ static void gap_params_init(void)
                                           strlen(DEVICE_NAME));
     APP_ERROR_CHECK(err_code); // Check for errors
 
-    // Always initialize all fields in structs to zero or you might get unexpected behaviour
-//    memset(&gap_conn_params, 0, sizeof(gap_conn_params));
-
-    // Populate the GAP connection parameter struct
-//    gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
-//    gap_conn_params.max_conn_interval = MAX_CONN_INTERVAL;
-//    gap_conn_params.slave_latency     = SLAVE_LATENCY;
-//    gap_conn_params.conn_sup_timeout  = CONN_SUP_TIMEOUT;
-
-    // Set GAP Peripheral Preferred Connection Parameters
-    // The device use these prefered values when negotiating connection terms with another device
-    //err_code = sd_ble_gap_ppcp_set(&gap_conn_params);
-    //APP_ERROR_CHECK(err_code);
-                                          
-
     err_code = sd_ble_gap_appearance_set(0);  // Set appearence	这里的appearance就可以安字面意思理解，外貌。
-    //sd_ble_gap_appearance_set(BLE_APPEARANCE_HID_MOUSE); //自己加的
     APP_ERROR_CHECK(err_code);// Check for errors
 
     err_code =  sd_ble_gap_tx_power_set	(-40); //设置信号发射强度
     APP_ERROR_CHECK(err_code);// Check for errors
 
-    /*//下面是另外加的， to set the address type to “Private Resolvable address”
-	ble_gap_addr_t gap_address;
-	gap_address.addr_type = BLE_GAP_ADDR_TYPE_RANDOM_PRIVATE_RESOLVABLE;
-	err_code = sd_ble_gap_address_set(BLE_GAP_ADDR_CYCLE_MODE_AUTO, &gap_address);
-	APP_ERROR_CHECK(err_code);// Check for errors
-	*/
-}
-
-
-/**@brief Function for handling the Connection Parameters Module.
- *
- * @details This function will be called for all events in the Connection Parameters Module which
- *          are passed to the application.
- *          @note All this function does is to disconnect. This could have been done by simply
- *                setting the disconnect_on_fail config parameter, but instead we use the event
- *                handler mechanism to demonstrate its use.
- *
- * @param[in] p_evt  Event received from the Connection Parameters Module.
- */
-static void on_conn_params_evt(ble_conn_params_evt_t * p_evt)
-{
-    uint32_t err_code;
-
-    if (p_evt->evt_type == BLE_CONN_PARAMS_EVT_FAILED)
-    {
-        err_code = sd_ble_gap_disconnect(m_conn_handle, BLE_HCI_CONN_INTERVAL_UNACCEPTABLE);
-        APP_ERROR_CHECK(err_code);
-    }
-}
-
-
-/**@brief Function for handling a Connection Parameters error.
- *
- * @param[in] nrf_error  Error code containing information about what went wrong.
- */
-static void conn_params_error_handler(uint32_t nrf_error)
-{
-    APP_ERROR_HANDLER(nrf_error);
-}
-
-
-/**@brief Function for initializing the Connection Parameters module.
- */
-static void conn_params_init(void)
-{
-    uint32_t               err_code;
-    ble_conn_params_init_t cp_init;
-
-    memset(&cp_init, 0, sizeof(cp_init));
-
-    cp_init.p_conn_params                  = NULL;
-    cp_init.first_conn_params_update_delay = FIRST_CONN_PARAMS_UPDATE_DELAY;
-    cp_init.next_conn_params_update_delay  = NEXT_CONN_PARAMS_UPDATE_DELAY;
-    cp_init.max_conn_params_update_count   = MAX_CONN_PARAMS_UPDATE_COUNT;
-    cp_init.start_on_notify_cccd_handle    = BLE_GATT_HANDLE_INVALID;
-    cp_init.disconnect_on_fail             = false;
-    cp_init.evt_handler                    = on_conn_params_evt;
-    cp_init.error_handler                  = conn_params_error_handler;
-
-    err_code = ble_conn_params_init(&cp_init);
-    APP_ERROR_CHECK(err_code);
 }
 
 
@@ -459,28 +375,7 @@ static void device_manager_init(bool erase_bonds)
 
 
 /**@brief Function for initializing the Advertising functionality.
-
-static void advertising_init(void)
-{
-    uint32_t      err_code;
-    ble_advdata_t advdata; // Struct containing advertising parameters
-
-    // Build advertising data struct to pass into @ref ble_advertising_init.
-    memset(&advdata, 0, sizeof(advdata));
-
-  //advdata.name_type               = BLE_ADVDATA_FULL_NAME;
-    advdata.name_type               = BLE_ADVDATA_SHORT_NAME; // Use a shortened name 改动
-    advdata.short_name_len          = 6; // Advertise only first 6 letters of name 改动
-    advdata.flags                   = BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE;
-
-    ble_adv_modes_config_t options = {0};
-    options.ble_adv_fast_enabled  = BLE_ADV_FAST_ENABLED;
-    options.ble_adv_fast_interval = APP_ADV_INTERVAL;
-    options.ble_adv_fast_timeout  = APP_ADV_TIMEOUT_IN_SECONDS;
-
-    err_code = ble_advertising_init(&advdata, NULL, &options, on_adv_evt, NULL);
-    APP_ERROR_CHECK(err_code);
-}*/
+*/
 static void advertising_init(void)
 {	//怎么更改到non－connectable 的广播种类啊？ 好像这里的广播用的不是softdevice，试试用softdevice。
     uint32_t      err_code;
@@ -582,7 +477,6 @@ int main(void)
     device_manager_init(erase_bonds);
     gap_params_init();
     advertising_init();
-    conn_params_init();
 
     // Start execution.
     err_code = ble_advertising_start(BLE_ADV_MODE_FAST);
